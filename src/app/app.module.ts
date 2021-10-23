a239894819ec712669f2e6f8dd9a816e93d9ce0f
daniel lizana <daniel.lizana01@gmail.com>
2021-10-23 17:41:22 -0500

    agregando componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './component/login/login.component';
+import { NavComponent } from './component/nav/nav.component';
+import { GaleryComponent } from './component/galery/galery.component';
+import { FormComponent } from './component/form/form.component';
+import { FootComponent } from './component/foot/foot.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavComponent,
+    GaleryComponent,
+    FormComponent,
+    FootComponent
   ],
   imports: [
     BrowserModule,
